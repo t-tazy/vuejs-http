04234bba0fe8bb345d662b14f2953e121a5989d3
t-tazy <takumin390511@gmail.com>
2021-11-24 22:47:23 +0900

    postメソッド getメソッド

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,57 @@
 <template>
   <div id="app">
     <h3>掲示板に投稿する</h3>
-    <label for"name">ニックネーム</label>
+    <label for="name">ニックネーム</label>
     <input id="name" type="text" v-model="name">
     <br><br>
     <label for="comment">コメント：</label>
-    <textarea id="comment"></textarea>
+    <textarea id="comment" v-model="comment"></textarea>
     <br><br>
-    <button>コメントをサーバーに送る</button>
+    <button @click="createComment">コメントをサーバーに送る</button>
     <h2>掲示板</h2>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       name: "",
       comment: ""
+    }
+  },
+  created() {
+    axios.get(
+      'https://firestore.googleapis.com/v1/projects/vuejs-http-30f51/databases/(default)/documents/comments'
+    )
+    .then(response => {
+      console.log(response);
+    });
+  },
+  methods: {
+    createComment() {
+      axios.post(
+        'https://firestore.googleapis.com/v1/projects/vuejs-http-30f51/databases/(default)/documents/comments',
+        {
+          fields: {
+            name: {
+              stringValue: this.name
+            },
+            comment: {
+              stringValue: this.comment
+            }
+          }
+        }
+      )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+      this.name = "";
+      this.comment = "";
     }
   }
 }
