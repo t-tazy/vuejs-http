284f621df6a40f78afe86fcf685e18c4e9ea7254
t-tazy <takumin390511@gmail.com>
2021-11-25 15:06:26 +0900

    interseptors

--- src/main.js
+++ src/main.js
@@ -6,6 +6,26 @@ Vue.config.productionTip = false
 
 axios.defaults.baseURL = 'https://firestore.googleapis.com/v1/projects/vuejs-http-30f51/databases/(default)/documents'
 
+const interceptorsRequest = axios.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+const interceptorsResponse = axios.interceptors.response.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.request.eject(interceptorsRequest);
+axios.interceptors.response.eject(interceptorsResponse);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
